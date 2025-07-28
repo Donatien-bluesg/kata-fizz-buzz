d27678a1cb903343e29f456abf307f43500861f0
Donatien <donatien.couka@bluesg.com.sg>
2025-07-28 12:06:17 +0800

    ♻️ refactor fizz logic

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,15 +1,21 @@
 import { EmptyValueError } from "./errors/empty-value.error";
 import { ValueTooLowError } from "./errors/valuye-to-low.error";
 
+const SERIES_START = 1;
+
+const FIZZ_NUMBER = 3;
+const FIZZ_NAME = "Fizz";
+
 export function fizzbuzz(value: number): string {
   if (!value) throw new EmptyValueError();
-  if (value < 1) throw new ValueTooLowError();
+  if (value < SERIES_START) throw new ValueTooLowError();
 
   let result = "";
 
-  for (let i = 1; i <= value; i++) {
-    if (i > 1) result += " ";
-    if (i % 3 === 0) result += "Fizz";
+  for (let i = SERIES_START; i <= value; i++) {
+    if (i > SERIES_START) result += " ";
+
+    if (i % FIZZ_NUMBER === 0) result += FIZZ_NAME;
     else result += i.toString();
   }
 
